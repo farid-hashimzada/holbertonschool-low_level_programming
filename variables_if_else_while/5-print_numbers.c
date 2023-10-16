65ef1d88d3b3c2e10aa00b055df931df81e0fe2c
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-10-16 07:41:12 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x;
+	for(x = 0; x <= 10; x++)
+	{
+		putchar(x);
+	}
+		putchar('\n');
+		return (0);
+}
