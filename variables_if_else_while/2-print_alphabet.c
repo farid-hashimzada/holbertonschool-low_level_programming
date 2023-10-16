f3d5c8c07a402001e1f2e90e99eda524c5ceee78
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-10-16 05:53:46 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this program prints alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+alphabet++;
+}
+
+putchar('\n');
+return (0);
+}
