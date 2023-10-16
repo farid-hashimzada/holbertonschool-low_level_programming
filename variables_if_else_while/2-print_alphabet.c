f8c0a018d283aa1fddcd0ddec3ef495b48ef3229
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-10-16 06:02:40 -0700

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,12 @@
 
 int main(void)
 {
-char alphabet = 'a';
-while (alphabet <= 'z')
-{
-alphabet++;
-}
-
-putchar('\n');
-return (0);
+	char alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
 }
