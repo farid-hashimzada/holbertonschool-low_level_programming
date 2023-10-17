fd0e858e8b7b09643278c4d5de85604341d1f583
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-10-17 00:43:22 -0700

    6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,11 @@
  */
 int main(void)
 {
-        int x;
-
-        for (x = 0; x < 10; x++)
-        {
-                putchar(x + '0');
-        }
-                putchar('\n');
-                return (0);
+int x;
+for (x = 0; x < 10; x++)
+{
+	putchar(x + '0');
+}
+putchar('\n');
+return (0);
 }
