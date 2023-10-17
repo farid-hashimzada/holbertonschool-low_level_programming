4031f4f30f0618d56f959c8a64cc407de7eb9392
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-10-17 00:19:30 -0700

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        int x;
+
+        for (x = 0; x < 10; x++)
+        {
+                putchar(x + '0');
+        }
+                putchar('\n');
+                return (0);
+}
