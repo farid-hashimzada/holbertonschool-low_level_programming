c033d39253c4e81501127688190502e380f37f4b
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-10-18 05:32:51 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abc - Write a function that computes the absolute value of an integer.
+ *@n:Write a function that computes the absolute value of an integer.
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return ((-1) * n);
+}
