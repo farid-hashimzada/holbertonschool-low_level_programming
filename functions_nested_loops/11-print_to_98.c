a9ffcc1b07afff3c67dcd894d5074ab8e17c26e9
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-10-18 10:02:38 -0700

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,47 +1,44 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * print_to_98 - prints all natural numbers from n to 98, followed by a new line.
- *@n: parametr.
- * Return: Always 0.
- */
 
+/**
+  * print_to_98 - Prints all natural numbers from n to 98
+  * @n: The number to start printing from
+  *
+  * Return: Always 0.
+  */
 void print_to_98(int n)
 {
-	int i, j;
-
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (; n <= 98; n++)
 		{
-			if (i!= 98)
+			if (n == 98)
 			{
-				printf("%d", i);
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				printf("%d", n);
+				printf("\n");
+				break;
 			}
-			else if (i == 98)
+			else
 			{
-				printf("%d\n", i);
-				_putchar(',');
-				_putchar(' ');
+				printf("%d, ", n);
 			}
 		}
 	}
-	else if (n >= 98)
+	else
 	{
-		for (j = n; j >= 98; j--)
+		for (; n >= 98; n--)
 		{
-			if (j!= 98)
+			if (n == 98)
 			{
-				printf("%d,", j);
+				printf("%d", n);
+				printf("\n");
+				break;
 			}
-			else if (j == 98)
+			else
 			{
-				printf("%d\n",j);
+				printf("%d, ", n);
 			}
 		}
 	}
 }
-
