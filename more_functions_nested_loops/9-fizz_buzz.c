7053968c2f98b0d2a1b43d9dc9930ea4c754a5d0
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-10-20 11:43:44 -0700

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,23 +8,21 @@
 
 int main(void)
 {
-	int i;
+	int i, n = 100;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= n; i++)
 	{
 		if (i % 15 == 0)
+			printf(" FizzBuzz");
+		else if ((i % 3) == 0)
+			printf(" Fizz");
+		else if ((i % 5) == 0)
+			printf(" Buzz");
+		else
 		{
-			printf("Fizz");
-			printf(" ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Buzz");
-			printf(" ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("FizzBuzz");
+			if (i != 1)
+				printf(" ");
+			printf("%d", i);
 		}
 	}
 	return (0);
