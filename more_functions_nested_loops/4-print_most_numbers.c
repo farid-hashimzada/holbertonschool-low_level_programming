8acb70bb40ecffe2f4b73f3a29d1858fe8b02663
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-10-20 08:35:29 -0700

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers,from 0 to 9,followed by a new line.
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		_putchar(x);
+	}
+	_putchar('\n');
+}
