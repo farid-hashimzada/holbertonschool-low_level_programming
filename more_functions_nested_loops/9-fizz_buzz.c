906c5e7482cc578aa018c723d5947e2a59455305
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-10-20 11:27:35 -0700

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 15 == 0) 
+		{
+			printf("Fizz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+	}
+	return (0);
+}
