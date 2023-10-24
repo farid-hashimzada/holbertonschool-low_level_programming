82eba8a29d2e5bdc7ac87704996a754cc6d15929
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-10-24 07:03:16 -0700

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_array - print value of array.
+ * @a: array.
+ * @n: length of ar
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+	}
+	putchar('\n');
+}
