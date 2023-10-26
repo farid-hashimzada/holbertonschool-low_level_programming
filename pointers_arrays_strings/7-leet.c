503a11ddd3c8dd59694963eab4c2d5d90c29b265
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-10-26 08:17:05 -0700

    7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -27,11 +27,12 @@ char *leet(char *s) {
     return str;
 }
 
-int main(void) {
+int main(void)
+{
     char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
     char *p;
 
     p = leet(s);
     printf("%s", p);
-    return (0);
-}
+    printf("%s", s);
+   
