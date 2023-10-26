f0067bcf47f02a251cbff700f6a74b9b779e585c
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-10-26 08:33:42 -0700

    7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -2,12 +2,12 @@
 
 /**
  *leet - 1337
- *@s: 1337
+ *@bush: 1337
  *
  * Return: Always 0.
  */
 
-char *leet(char *s) {
+/*char *leet(char *s) {
 	char *str = s;
 
 	while (*s)
@@ -35,4 +35,23 @@ char *leet(char *s) {
 	s++;
 	}
 	return str;
+}*/
+
+char *leet(char *bush)
+{
+	int i, j;
+	char letter[] = { 'a', 'e', 'o', 't', 'l' };
+	char letterUP[] = { 'A', 'E', 'O', 'T', 'L' };
+	char number[] = { 4, 3, 0, 7, 1 };
+
+	for (i = 0; bush[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (bush[i] == letter[j] || bush[i] == letterUP[j])
+				bush[i] = number[j] + '0';
+		}
+	}
+
+	return (bush);
 }
