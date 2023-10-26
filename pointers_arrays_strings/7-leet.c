d4910cd15f40b13bf9a361d00e8c3b1cd6d7614d
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-10-26 08:28:13 -0700

    7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,20 +8,25 @@
  */
 
 char *leet(char *s) {
-    char *str = s;
-    while (*s) {
-        if ((*s == 'a' || *s == 'A')) {
-            *s = '4';
-        } else if ((*s == 'e' || *s == 'E')) {
-            *s = '3';
-        } else if ((*s == 'o' || *s == 'O')) {
-            *s = '0';
-        } else if ((*s == 't' || *s == 'T')) {
-            *s = '7';
-        } else if ((*s == 'l' || *s == 'L')) {
-            *s = '1';
-        }
-        s++;
-    }
-    return str;
+	char *str = s;
+
+	while (*s) {
+	if ((*s == 'a' || *s == 'A')) {
+		*s = '4';
+	}
+	else if ((*s == 'e' || *s == 'E')) {
+		*s = '3';
+	}
+	else if ((*s == 'o' || *s == 'O')) {
+		*s = '0';
+	}
+	else if ((*s == 't' || *s == 'T')) {
+		*s = '7';
+	}
+	else if ((*s == 'l' || *s == 'L')) {
+		*s = '1';
+	}
+	s++;
+	}
+	return str;
 }
