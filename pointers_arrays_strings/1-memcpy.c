791680bd01d53c569df9be5b54c690399cd1f9e4
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-10-27 07:37:29 -0700

    1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * _memcpy - Write a function that copies memory area.
+ * @dest: parametr
+ * @src: parametr
+ * @n: parametr
  *
  * Return: Nothing.
  */
