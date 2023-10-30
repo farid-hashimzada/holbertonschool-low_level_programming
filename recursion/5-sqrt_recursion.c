b1a209b99983e4ad51632adfb29dcf8258c3c2aa
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-10-30 07:50:54 -0700

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -9,27 +9,35 @@
  * Return: Always 0.
  */
 
+int _sqrt(int n, int guess)
+{
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+	if (guess * guess > n)
+	{
+		return (-1);
+	}
+	return (_sqrt(n, guess + 1));
+}
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to calculate the square root of.
+ *
+ * Return:The square root of n, or -1 if it doesn't have a natural square root.
+ */
+
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
 		return (-1);
 	}
-	else if (n == 0 || n == 1)
+	if (n == 0)
 	{
-		return (n);
+		return (0);
 	}
-	else
-	{
-		int i;
-
-		for (i = 1; i <= n / 2; i++)
-		{
-			if (i * i == n)
-			{
-				return (i);
-			}
-		}
-		return (-1);
-	}
+	return (_sqrt(n, 1));
 }
