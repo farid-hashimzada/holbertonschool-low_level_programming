b30f2eee622f6f1cc8831474f4b32b90e51489e2
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-11-01 06:30:15 -0700

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -5,17 +5,17 @@
  * @argc: size of arqument
  * @argv: array
  *
- *
+ * Return: always 0
  *
  */
 
 int main(int argc, char *argv[])
 {
-  int i;
+	int i;
 
-  for (i = 0; i < argc; i++)
-    {
-      printf("%s\n", argv[i]);
-    }
-  return (0);
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+	return (0);
 }
