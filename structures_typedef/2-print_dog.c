d0a762369aa81ada22c32e4d7d8e185c9a40a239
farid-hashimzada <hashimzadafarid@gmail.com>
2023-11-20 07:27:36 -0800

    2-print_dog.c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - dog print
+ *
+ * @d: struct dog pointer
+ *
+ * Return: Always 0.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL || d->owner == NULL)
+		{
+			d->name = "(nill)";
+			d->age = 0.0;
+			d->owner = "(nill)";
+		}
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+
+}
