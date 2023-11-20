325fb5541503be5f5003c1ad296b0b794a83bbf6
farid-hashimzada <hashimzadafarid@gmail.com>
2023-11-20 08:28:52 -0800

    5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - all dogs free
+ * @d: do freeee
+ *
+ * Return: Always 0.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
