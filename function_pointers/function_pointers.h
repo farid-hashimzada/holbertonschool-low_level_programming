f5c8f33090380784a3f011cc3285749808d62150
farid-hashimzada <hashimzadafarid@gmail.com>
2023-11-22 05:40:52 -0800

    0-print_name.c

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,8 @@
+#ifndef SCHOOL_F
+#define SCHOOL_F
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
+#endif
