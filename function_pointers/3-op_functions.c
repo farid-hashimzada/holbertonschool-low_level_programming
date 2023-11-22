80bf2db1fdae88f4d14523eb18d0911c8f876f7e
farid-hashimzada <hashimzadafarid@gmail.com>
2023-11-22 08:50:10 -0800

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * op_add - sum operator
+ * @a: one parametr
+ * @b: two
+ *
+ * Return: a+b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction
+ * @a: sub_onw
+ * @b: sub_two
+ *
+ * Return: a-b
+ */
+int op_sub(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_mul - subtraction
+ * @a: mul_onw
+ * @b: mul_two
+ *
+ * Return: a*b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - subtraction
+ * @a: div_onw
+ * @b: div_two
+ *
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - subtraction
+ * @a: mod_onw
+ * @b: mod_two
+ *
+ * Return: a / b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
