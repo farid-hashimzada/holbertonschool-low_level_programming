70462eb62d6295c8df340df9e358f6858ccadc13
farid-hashimzada <hashimzadafarid@gmail.com>
2023-11-24 07:09:01 -0800

    1-print_numbers.c

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - sum the argument with separator
+ * @separator: arg
+ * @b: parametr
+ *
+ * Return: co
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list co;
+	unsigned int i;
+	int f;
+
+	va_start(co, n);
+	for (i = 0; i < n; i++)
+	{
+		f = va_arg(co, int);
+		if (separator != NULL && i != 0)
+			printf("%s%d", separator, f);
+		else
+			printf("%d", f);
+	}
+	va_end(co);
+	printf("\n");
+}
