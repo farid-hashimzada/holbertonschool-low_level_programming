e522c07679fdea1442ebbe6bd51868fd29e35a35
farid-hashimzada <hashimzadafarid@gmail.com>
2023-12-09 13:28:51 -0800

    0-binary_to_uint.c

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * binary_to_uint - convert binary to int.
+ * @b: string.
+ *
+ * Return: result
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+
+		result <<= 1;
+
+		if (b[i] == '1')
+			result += 1;
+	}
+
+	return (result);
+}
