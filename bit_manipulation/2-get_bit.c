0b7f9c26a1fa1db06993e3c24028c9dfd3afa431
farid-hashimzada <hashimzadafarid@gmail.com>
2023-12-10 00:48:36 -0800

    2-get_bit.c

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * get_bit - Getting bit of number.
+ * @n: number.
+ * @index: parametr
+ *
+ * Return: result
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int result = 0;
+	unsigned int i;
+
+	if (index > 31)
+		return (-1);
+
+	for (i = 0; i < index; i++)
+		n >>= 1;
+
+	result = n & 1;
+	return (result);
+}
