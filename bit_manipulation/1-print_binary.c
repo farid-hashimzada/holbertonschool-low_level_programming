3339e2e25c67cb6c0e75d5aae30937e36500f268
farid-hashimzada <hashimzadafarid@gmail.com>
2023-12-10 00:45:44 -0800

    1-print_binary.c

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * print_binary - Print binary numbers.
+ * @n:number.
+ */
+
+void print_binary(unsigned long int n)
+{
+	int result = 0;
+
+	if (n > 1)
+		print_binary(n >> 1);
+
+	result = n & 1;
+	putchar('0' + result);
+}
