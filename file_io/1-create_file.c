41d31273fd71637ac5d92618d91222422ce0178b
farid-hashimzada <hashimzadafarid@gmail.com>
2023-12-14 05:20:26 -0800

    1-create_file.c

---
+++ file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * create_file - creating file.
+ * @filename: name file.
+ * @text_content: text.
+ *
+ * Return: 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w;
+	int len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+		{
+			len++;
+		}
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+	{
+		return (-1);
+	}
+	close(o);
+
+	return (1);
+}
