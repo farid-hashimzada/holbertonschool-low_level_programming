06cd1d2c8da352537ca10bc2b0ead0f4b717bb8c
farid-hashimzada <hashimzadafarid@gmail.com>
2023-12-15 05:57:27 -0800

    0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash->size = size;
 	hash->array = malloc(sizeof(hash_node_t *) * size);
-	
+
 	if (hash->array == NULL)
 		return (NULL);
 
