03fd0c351dbe0ed3100aa50af4a14dee50a6ece8
farid-hashimzada <hashimzadafarid@gmail.com>
2023-12-17 04:31:34 -0800

    3-hash_table_set.c

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+/**
+ * hash_table_set - Set element to hash table.
+ * @ht: our hash table.
+ * @key: key.
+ * @value: value.
+ *
+ * Return: 1 succes -1 fail.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new = malloc(sizeof(hash_node_t));
+
+	if (key == NULL || value == NULL || new == NULL || ht == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = NULL;
+
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new;
+	}
+	else
+	{
+		if (strcmp(ht->array[index]->key, key) == 0)
+		{
+			free(ht->array[index]->value);
+			ht->array[index]->value = strdup(value);
+			free(new->value);
+			free(new->key);
+			free(new);
+		}
+		else
+		{
+			new->next = ht->array[index];
+			ht->array[index] = new;
+		}
+	}
+
+	return (1);
+}
