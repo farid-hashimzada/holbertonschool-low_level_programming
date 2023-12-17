f10949751771cb1ad2c9a560103c05c41e81020d
farid-hashimzada <hashimzadafarid@gmail.com>
2023-12-17 04:33:34 -0800

    4-hash_table_get.c

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "hash_tables.h"
+/**
+ * hash_table_get - Find element according the key.
+ * @ht: hash table.
+ * @key: key value.
+ *
+ * Return:  the value associated with the element
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	while (ht->array[index])
+	{
+		if (ht->array[index] && strcmp(ht->array[index]->key, key) == 0)
+			return (ht->array[index]->value);
+		ht->array[index] = ht->array[index]->next;
+	}
+
+	return (NULL);
+}
