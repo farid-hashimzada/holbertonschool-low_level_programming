9289d9fafbfcac9bad24dcf0f8cf1e3aaf0c3792
farid-hashimzada <hashimzadafarid@gmail.com>
2024-01-19 07:08:35 -0800

    1-binary search algorithm

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * binary_search - dvide two side on the array
+ * @array: list of the array
+ * @size: size of the array
+ * @value: value of array
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int low, high, mid, j;
+
+	if (array == NULL)
+		return (-1);
+
+	low = 0;
+	high = size - 1;
+
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		printf("Searching in array: ");
+
+		for (j = low; j <= high; j++)
+		{
+			printf("%d", array[j]);
+			if (j < high)
+				printf(", ");
+		}
+		printf("\n");
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+	return (-1);
+
+}
