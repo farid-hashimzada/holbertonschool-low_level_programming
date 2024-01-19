91b380ecc5d3bffe0ab92ce05c69d2370848e29c
farid-hashimzada <hashimzadafarid@gmail.com>
2024-01-19 06:30:16 -0800

    0-linear algorithm

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -5,7 +5,7 @@
  * linear_search - linear search algorithm
  * @array: firt element of the array
  * @size: array size
- * @value: value 
+ * @value: value
  *
  */
 
@@ -21,7 +21,6 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
-		
 	}
 	return (-1);
 }
